b94209ba9b77d2af56a062e74c2f7418938f1428
Liutsing Robert <liutsingluo@gmail.com>
2023-09-21 20:36:29 +0800

    init commit

---
+++ main.py
@@ -0,0 +1,132 @@
+import torch
+
+from torchvision import datasets, transforms
+
+# Hyper parameters
+learning_rate = 0.001
+sequence_length = 28
+hidden_size = 128
+num_classes = 10
+batch_size = 64
+input_size = 28
+num_layers = 2
+num_epochs = 3
+
+train_dataset = datasets.MNIST(
+    root="./data/training", train=True, transform=transforms.ToTensor(), download=True
+)
+test_dataset = datasets.MNIST(
+    root="./data/testing", train=False, transform=transforms.ToTensor(), download=True
+)
+
+train_loader = torch.utils.data.DataLoader(
+    dataset=train_dataset, batch_size=batch_size, shuffle=True
+)
+test_loader = torch.utils.data.DataLoader(
+    dataset=test_dataset, batch_size=batch_size, shuffle=False
+)
+
+
+class RNN(torch.nn.Module):
+    def __init__(self, input_size, hidden_size, num_layers, num_classes):
+        super(RNN, self).__init__()
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.lstm = torch.nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
+        self.fc = torch.nn.Linear(hidden_size, num_classes)
+
+    def forward(self, x):
+        h = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(device)
+        c = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(device)
+
+        out, _ = self.lstm(x, (h, c))
+
+        out = self.fc(out[:, -1, :])
+        return out
+
+
+def get_default_device():
+    """Pick GPU if available, else CPU"""
+    if torch.cuda.is_available():
+        return torch.device("cuda")
+    else:
+        return torch.device("cpu")
+
+
+def to_device(data, device):
+    """Move tensor(s) to chosen device"""
+    if isinstance(data, (list, tuple)):
+        return [to_device(x, device) for x in data]
+    return data.to(device, non_blocking=True)
+
+
+class DeviceDataLoader:
+    """Wrap a dataloader to move data to a device"""
+
+    def __init__(self, dl, device):
+        self.dl = dl
+        self.device = device
+
+    def __iter__(self):
+        """Yield a batch of data after moving it to device"""
+        for b in self.dl:
+            yield to_device(b, self.device)
+
+
+def __len__(self):
+    """Number of batches"""
+    return len(self.dl)
+
+
+device = get_default_device()
+print(device)
+
+model = RNN(input_size, hidden_size, num_layers, num_classes)
+to_device(model, device)
+
+
+criterion = torch.nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+
+# Train the model
+total_step = len(train_loader)
+for epoch in range(num_epochs):
+    for i, (images, labels) in enumerate(train_loader):
+        images = images.reshape(-1, sequence_length, input_size).to(device)
+        labels = labels.to(device)
+
+        # Forward pass
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+
+        # Backward pass and optimize
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if (i + 1) % 100 == 0:
+            print(
+                "Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}".format(
+                    epoch + 1, num_epochs, i + 1, total_step, loss.item()
+                )
+            )
+
+
+# Evaluate the model
+model.eval()
+with torch.no_grad():
+    right = 0
+    total = 0
+    for images, labels in test_loader:
+        images = images.reshape(-1, sequence_length, input_size).to(device)
+        labels = labels.to(device)
+        outputs = model(images)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        right += (predicted == labels).sum().item()
+print(
+    "Test Accuracy of the model on the 10000 test images: {} %".format(
+        100 * right / total
+    )
+)
